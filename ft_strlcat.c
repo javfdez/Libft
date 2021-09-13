72ce5dc1dea5c8e5f47bd4c331a1fa5f46fc4b9c
Javier Fernandez fernandez <javferna@c1r12s4.42madrid.com>
2021-09-13 19:36:19 +0200

    ft_strlcat 3tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: javferna <javferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 12:09:55 by javferna          #+#    #+#             */
-/*   Updated: 2021/09/13 16:45:33 by javferna         ###   ########.fr       */
+/*   Updated: 2021/09/13 19:26:41 by javferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	len_dest = ft_strlen(dst);
 	len_src = ft_strlen(src);
-	if (dstsize < len_dest)
+	if (dstsize < len_dest || !dstsize)
 		return (dstsize + len_src);
 	i = len_dest;
 	j = 0;
