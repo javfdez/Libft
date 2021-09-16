17f9e69aea0c4391791feede79c773b8ff177b79
Javiff8 <javi.ff.978@gmail.com>
2021-09-17 00:31:20 +0200

    ft_strtrim improved

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,11 @@
 /*   By: javferna <javferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 12:15:26 by javferna          #+#    #+#             */
-/*   Updated: 2021/09/16 00:25:41 by javferna         ###   ########.fr       */
+/*   Updated: 2021/09/17 00:15:14 by javferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_charinset(char c, char const *set)
-{
-	int	i;
-
-	i = -1;
-	while (set[++i])
-	{
-		if (set[i] == c)
-			return (1);
-	}
-	return (0);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -37,12 +24,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (NULL);
 	i = 0;
-	while (s1[i] && ft_charinset(s1[i], set))
+	while (s1[i] && ft_strrchr(set, s1[i]))
 		i++;
 	len = ft_strlen(s1);
 	if (i == len)
 		return (ft_strdup(""));
-	while (len - 1 > 0 && ft_charinset(s1[len - 1], set))
+	while (len - 1 > 0 && ft_strrchr(set, s1[len - 1]))
 		len--;
 	str = malloc((len - i + 1) * sizeof(char));
 	if (!str)
